ba1961fdb1630c377c1883d92d16d6516534fb11
Vaclav Telensky <vaclav.telensky@dixonscarphone.com>
2016-11-27 10:41:14 +0100

    Initial commit - entrence task solution

---
+++ entrenceTask/find_combinations.py
@@ -0,0 +1,9 @@
+from flightsSearcherService import FlightsSearcherService
+import sys
+
+flights = sys.stdin.read().split()
+
+flightsSearcherService = FlightsSearcherService(flights[1::])
+flightsSearcherService.printFlightCombination(
+	flightsSearcherService.findCombination()
+)
---
+++ entrenceTask/flightsSearcherService.py
@@ -0,0 +1,57 @@
+from flight import Flight
+from itinerary import Itinerary
+
+class FlightsSearcherService:
+
+	def __init__(self, flightsData):
+		self.flightsData = flightsData
+
+	def findCombination(self):
+		flightCombinations = []
+		itineraryQueue = []
+
+		for baggageCount in [0, 1, 2]:
+			itineraryQueue = self.getInitializedItineraryQueue(baggageCount)
+
+			while len(itineraryQueue) > 0:
+				itinerary = itineraryQueue.pop()
+				
+				for flightLine in self.flightsData:
+					flight = Flight(self.getFlightData(flightLine))
+					if baggageCount > 0 and baggageCount != flight.getAllowedBaggage():
+						continue
+					else:
+						if itinerary.isFollowedUp(flight) and itinerary.isCreateValidPath(flight):		
+							itineraryQueue.append(
+								itinerary.addNextStopToWay(flight, baggageCount)
+							)
+				
+				flightCombinations.append(itinerary)
+
+		return flightCombinations
+
+	def getFlightData(self, flightLine):
+		return flightLine.split(',')
+
+	def getInitializedItineraryQueue(self, baggageCount):
+		itineraryQueue = []
+
+		for flightLine in self.flightsData:
+				flight = Flight(self.getFlightData(flightLine))
+				if baggageCount == 0 or flight.getAllowedBaggage() >= baggageCount:
+					itineraryQueue.append(
+						Itinerary(
+							baggageCount, 
+							flight.getSource() + '->' + flight.getDestination(), 
+							[flight.getFlightNumber()], 
+							flight.getPrice(), 
+							flight.getArrivalTime()
+						)
+					)
+
+		return itineraryQueue
+
+	def printFlightCombination(self, combinations):
+		print('numberOfBaggage,itinerary,listOfFlights,wholePriceIncludingBaggage')
+		for itinerary in combinations:
+			print(itinerary.toString())
