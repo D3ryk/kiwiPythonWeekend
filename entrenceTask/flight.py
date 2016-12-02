352a8dc15bdcf5fa7b1f1c123c58b0859339da95
D3ryk <vaclav.telensky@seznam.cz>
2016-12-02 19:24:54 +0100

    adding private properties to Flight object

--- entrenceTask/flight.py
+++ entrenceTask/flight.py
@@ -3,35 +3,35 @@ from datetime import datetime
 
 class Flight:
     def __init__(self, flightData):
-        self.source = str(flightData[0])
-        self.destination = str(flightData[1])
-        self.departure = datetime.strptime(flightData[2], '%Y-%m-%dT%H:%M:%S')
-        self.arrival = datetime.strptime(flightData[3], '%Y-%m-%dT%H:%M:%S')
-        self.flight_number = str(flightData[4])
-        self.price = int(flightData[5])
-        self.bags_allowed = int(flightData[6])
-        self.bag_price = int(flightData[7])
+        self._source = str(flightData[0])
+        self._destination = str(flightData[1])
+        self._departure = datetime.strptime(flightData[2], '%Y-%m-%dT%H:%M:%S')
+        self._arrival = datetime.strptime(flightData[3], '%Y-%m-%dT%H:%M:%S')
+        self._flight_number = str(flightData[4])
+        self._price = int(flightData[5])
+        self._bags_allowed = int(flightData[6])
+        self._bag_price = int(flightData[7])
 
     def getAllowedBaggage(self):
-        return self.bags_allowed
+        return self._bags_allowed
 
     def getSource(self):
-        return self.source
+        return self._source
 
     def getDestination(self):
-        return self.destination
+        return self._destination
 
     def getFlightNumber(self):
-        return self.flight_number
+        return self._flight_number
 
     def getPrice(self):
-        return self.price
+        return self._price
 
     def getBagPrice(self):
-        return self.bag_price
+        return self._bag_price
 
     def getArrivalTime(self):
-        return self.arrival
+        return self._arrival
 
     def getDepartureTime(self):
-        return self.departure
+        return self._departure
