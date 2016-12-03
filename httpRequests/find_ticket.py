cd9af6d82fd398b0406e986002cf3201f244bf53
D3ryk <vaclav.telensky@seznam.cz>
2016-12-03 18:00:02 +0100

    Argument processing of script find_ticket.py

--- httpRequests/find_ticket.py
+++ httpRequests/find_ticket.py
@@ -1,8 +1,16 @@
 from ticket_searcher import TicketSearcher
 from datetime import datetime
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--from", type=str, help="source station of searched connection")
+parser.add_argument("--to", type=str, help="destination station of searched connection")
+parser.add_argument("--when", type=str, help="date of searched connection")
+args = vars(parser.parse_args())
 
 ts = TicketSearcher()
 
-connections = ts.get_connections(ts.get_city_id('Praha'), ts.get_city_id('Brno'), datetime.strptime('2016-12-25', '%Y-%m-%d'))
+connections = ts.get_connections(ts.get_city_id(args['from']), ts.get_city_id(args['to']), datetime.strptime(args['when'], '%Y-%m-%d'))
 best_connection = ts.get_best_connection(connections)
 print(best_connection)
