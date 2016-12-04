d6c579684550ee696e47e98afd01babc50834896
D3ryk <vaclav.telensky@seznam.cz>
2016-12-04 11:28:32 +0100

    Script argument -w checks improvement + typo

--- ticketSearcher/find_ticket.py
+++ ticketSearcher/find_ticket.py
@@ -1,21 +1,37 @@
 from ticket_searcher import TicketSearcher
-from datetime import datetime
+from datetime import datetime, date
 import argparse
 
+# script argument setup
 parser = argparse.ArgumentParser()
 
 required = parser.add_argument_group('required arguments')
 required.add_argument("-f", "--from", type=str, help="source station of searched connection", required=True)
 required.add_argument("-t", "--to", type=str, help="destination station of searched connection", required=True)
 
-parser.add_argument("--when", type=str, help="date of searched connection")
+parser.add_argument("-w", "--when", type=str, help="date of searched connection")
 args = vars(parser.parse_args())
 
-if not args['when']:
-    args['when'] = datetime.today().strftime('%Y-%m-%d')
+# --when/-w parameter sanitation
+if args['when']:
+    try:
+        args['when'] = datetime.strptime(args['when'], '%Y-%m-%d')
+    except ValueError:
+        exit('Bad date format for parameter --when/-w should be in %Y-%m-%d')
 
+    if datetime.date(args['when']) < date.today():
+        exit('Provided date is from past')
+else:
+    args['when'] = datetime.today()
+
+# main logic
 ts = TicketSearcher()
 
-connections = ts.get_connections(ts.get_city_id(args['from']), ts.get_city_id(args['to']), datetime.strptime(args['when'], '%Y-%m-%d'))
+connections = ts.get_connections(
+    ts.get_city_id(args['from']),
+    ts.get_city_id(args['to']),
+    args['when']
+)
+
 best_connection = ts.get_best_connection(connections)
 print(best_connection)
